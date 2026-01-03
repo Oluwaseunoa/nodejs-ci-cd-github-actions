984b66d603a698aa99afa7246cbb350a52b6c373
Oluwaseun Osunsola <oluwaseun.beicks@gmail.com>
2026-01-03 14:56:26 +0100

    Updated the site content

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World - PR Test Successful!');
+  res.send('Hello World - PR Test Successful, CI/CD Pipeline is working!');
 });
 
 app.listen(port, () => {
